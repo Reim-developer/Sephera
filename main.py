3b5bbf8275645d308a88112e83c8ea0927dca31b
Reim-developer <contact.kaxtr@gmail.com>
2025-04-11 23:11:14 +0700

    Enhance tree command: improve help text and implement chart export functionality

--- main.py
+++ main.py
@@ -10,14 +10,44 @@ class Sephera:
         tree_command.add_argument(
             "--ignore",
             type = str,
-            help = "Regex pattern to ignore files or folders (e.g. '__pycache__|\\.git')",
+            help = "Regex pattern to ignore files or folders (e.g. --ignore '__pycache__|\\.git')",
+            default = None
+        )
+        tree_command.add_argument(
+            "--chart",
+            type = str,
+            nargs = "?",
+            const = "SepheraChart",
+            help = "Create chart for your directory tree (e.g --chart '<MyChartSaveDir>')",
             default = None
         )
         tree_command.set_defaults(function = self.walk_files)
 
     def walk_files(self, args) -> None:
         walker = WalkFile(args.ignore)
-        walker.show_list_tree()
+        stats = walker.show_list_tree()
+
+        if args.chart:
+            from chart.Exporter import Exporter
+            from rich.console import Console
+            from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn, TimeElapsedColumn
+
+            console = Console()
+            with Progress(
+                SpinnerColumn(), TextColumn("[progress.description]{task.description}"), 
+                BarColumn(bar_width = 30), TextColumn("{task.completed}/{task.total}"),
+                TimeElapsedColumn(), console = console, transient = True) as progress_bar:
+                task = progress_bar.add_task("Exporting Chart...", total = 4)
+
+                exporter = Exporter(args.chart)
+                exporter.export_file_tree_chart(
+                    files = stats["Files"],
+                    dirs = stats["Directory"],
+                    hidden_files = stats["Hidden_Files"],
+                    hidden_dirs = stats["Hidden_Directory"],
+                    on_step = lambda: progress_bar.update(task, advance = 1) 
+                )
+            print(f"Successfully created chart with name: {args.chart}.png")
 
 if __name__ == "__main__":
     cli = Sephera()
