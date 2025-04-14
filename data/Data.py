c812aae31c63ff539200b4b3bd2e50b4a6992d79
Reim-developer <contact.kaxtr@gmail.com>
2025-04-14 13:57:37 +0700

    Refactor Language class to LanguageData and enhance language configuration management

--- data/Data.py
+++ data/Data.py
@@ -14,7 +14,7 @@ class LanguageConfig:
     comment_style: CommentStyle
 
 
-class Languages:
+class LanguageData:
     def __init__(self) -> None:
         self._comment_styles: Dict[str, CommentStyle] = {
             "c_style": CommentStyle(
@@ -35,12 +35,34 @@ class Languages:
                 name = "Python",
                 extensions = [".py"],
                 comment_style = self._comment_styles["python_style"]
+            ),
+            LanguageConfig(
+                name = "Java",
+                extensions = [".java"],
+                comment_style = self._comment_styles["c_style"]
+            ),
+            LanguageConfig(
+                name = "C++",
+                extensions = [".cc", ".cxx", ".cpp", ".c++"],
+                comment_style = self._comment_styles["c_style"]
+            ),
+            LanguageConfig(
+                name = "JavaScript",
+                extensions = [".js"],
+                comment_style = self._comment_styles["c_style"]
+            ),
+            LanguageConfig(
+                name = "Shell Script",
+                extensions = [".sh"],
+                comment_style = self._comment_styles["shell_style"]
             )
         ]
 
+    @property
     def get_languages(self) -> List[LanguageConfig]:
         return self._languages
     
+    @property
     def get_comment_styles(self) -> Dict[str, CommentStyle]:
         return self._comment_styles
     
