43811e08c4d95bf9912cee44f2d75520bc3dbb95
Reim-developer <contact.kaxtr@gmail.com>
2025-04-14 18:15:09 +0700

    Refactor language configuration to use base64 encoded YAML and improve comment style handling

--- data/Data.py
+++ data/Data.py
@@ -1,5 +1,9 @@
+import yaml
+import base64
 from dataclasses import dataclass
 from typing import List, Optional, Dict
+
+BASE64_CONFIG_YAML = """IyBTZXBoZXJhIFByb2plY3Q6IFByb2dyYW1taW5nIExhbmd1YWdlIENvbmZpZy4KIyBUaGlzIHByb2plY3QgaXMgbGljZW5zZWQgdW5kZXIgdGhlIEdOVSBHZW5lcmFsIFB1YmxpYyBMaWNlbnNlIHYzLjAuCgpjb21tZW50X3N0eWxlczoKICBjX3N0eWxlOgogICAgc2luZ2xlX2xpbmU6ICIvLyIKICAgIG11bHRpX2xpbmVfc3RhcnQ6ICIvKiIKICAgIG11bHRpX2xpbmVfZW5kOiAiKi8iCgogIHB5dGhvbl9zdHlsZToKICAgIHNpbmdsZV9saW5lOiAiIyIKICAgIG11bHRpX2xpbmVfc3RhcnQ6ICciIiInCiAgICBtdWx0aV9saW5lX2VuZDogJyIiIicKICAKICBzaGVsbF9zdHlsZToKICAgIHNpbmdsZV9saW5lOiAiIyIKICAKbGFuZ3VhZ2VzOgogIC0gbmFtZTogUHl0aG9uCiAgICBleHRlbnNpb246CiAgICAgIC0gLnB5CiAgICBjb21tZW50X3N0eWxlczogcHl0aG9uX3N0eWxlCgogIC0gbmFtZTogSmF2YQogICAgZXh0ZW5zaW9uOgogICAgICAtIC5qYXZhCiAgICBjb21tZW50X3N0eWxlczogY19zdHlsZQogIAogIC0gbmFtZTogSmF2YVNjcmlwdAogICAgZXh0ZW5zaW9uOgogICAgICAtIC5qcwogICAgICAtIC5qc3gKICAgICAgLSAubWpzCiAgICBjb21tZW50X3N0eWxlczogY19zdHlsZQoKICAtIG5hbWU6IFNoZWxsIFNjcmlwdAogICAgZXh0ZW5zaW9uOgogICAgICAtIC5zaAogICAgY29tbWVudF9zdHlsZXM6IHNoZWxsX3N0eWxlCgogIC0gbmFtZTogQysrCiAgICBleHRlbnNpb246CiAgICAgIC0gLmNjCiAgICAgIC0gLmNwcAogICAgICAtIC5jeHgKICAgICAgLSAuYysrCiAgICBjb21tZW50X3N0eWxlczogY19zdHlsZQoKICAtIG5hbWU6IEdvbGFuZwogICAgZXh0ZW5zaW9uOgogICAgICAtIC5nbwogICAgY29tbWVudF9zdHlsZXM6IGNfc3R5bGUKCiAgLSBuYW1lOiBQZXJsCiAgICBleHRlbnNpb246CiAgICAgIC0gLnBsCiAgICBjb21tZW50X3N0eWxlczogc2hlbGxfc3R5bGUKCiAgLSBuYW1lOiBSdWJ5CiAgICBleHRlbnNpb246CiAgICAtIC5yYgogICAgY29tbWVudF9zdHlsZXM6IHNoZWxsX3N0eWxlCgogIC0gbmFtZTogQyBIZWFkZXIgRmlsZQogICAgZXh0ZW5zaW9uOgogICAgLSAuaAogICAgY29tbWVudF9zdHlsZXM6IGNfc3R5bGUKICAKICAtIG5hbWU6IEMrKyBIZWFkZXIgRmlsZQogICAgZXh0ZW5zaW9uOgogICAgLSAuaHBwCiAgICAtIC5oaAogICAgLSAuaCsrCiAgICBjb21tZW50X3N0eWxlczogY19zdHlsZQoKICAtIG5hbWU6IEMjCiAgICBleHRlbnNpb246CiAgICAtIC5jcwogICAgY29tbWVudF9zdHlsZXM6IGNfc3R5bGU="""
 
 @dataclass
 class CommentStyle:
@@ -11,51 +15,22 @@ class CommentStyle:
 class LanguageConfig:
     name: str
     extensions: List[str]
-    comment_style: CommentStyle
-
+    comment_style: str
 
 class LanguageData:
     def __init__(self) -> None:
-        self._comment_styles: Dict[str, CommentStyle] = {
-            "c_style": CommentStyle(
-                single_line = "//", 
-                multi_line_start = "/*", multi_line_end = "*/"
-            ),
-            "python_style": CommentStyle(
-                single_line = "#", 
-                multi_line_start = '"""', multi_line_end = '"""'
-            ),
-            "shell_style": CommentStyle(
-                single_line = "#"
-            )
-        }
+        yaml_source = base64.b64decode(BASE64_CONFIG_YAML).decode()
+        config_data = yaml.safe_load(yaml_source)
 
+        self._comment_styles: Dict[str, CommentStyle] = {
+            key: CommentStyle(**value) for key, value in config_data["comment_styles"].items()
+        }
         self._languages: List[LanguageConfig] = [
             LanguageConfig(
-                name = "Python",
-                extensions = [".py"],
-                comment_style = self._comment_styles["python_style"]
-            ),
-            LanguageConfig(
-                name = "Java",
-                extensions = [".java"],
-                comment_style = self._comment_styles["c_style"]
-            ),
-            LanguageConfig(
-                name = "C++",
-                extensions = [".cc", ".cxx", ".cpp", ".c++"],
-                comment_style = self._comment_styles["c_style"]
-            ),
-            LanguageConfig(
-                name = "JavaScript",
-                extensions = [".js"],
-                comment_style = self._comment_styles["c_style"]
-            ),
-            LanguageConfig(
-                name = "Shell Script",
-                extensions = [".sh"],
-                comment_style = self._comment_styles["shell_style"]
-            )
+                name = language["name"],
+                extensions = language["extension"],
+                comment_style = language["comment_styles"]
+            ) for language in config_data["languages"]
         ]
 
     @property
@@ -78,3 +53,6 @@ class LanguageData:
                 return language
         
         return None
+    
+    def get_comment_style(self, language: LanguageConfig) -> Optional[CommentStyle]:
+        return self._comment_styles.get(language.comment_style)
