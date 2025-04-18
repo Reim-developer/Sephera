b18e1a8213c1d285e67b752aa75ea09b4b9fdbef
Reim-developer <contact.kaxtr@gmail.com>
2025-04-18 22:32:06 +0700

    Refactor WalkFile class: streamline file walking logic and enhance error handling

--- sephera/WalkFile.py
+++ sephera/WalkFile.py
@@ -1,44 +1,41 @@
 import os
 import re
-from typing import Generator, Optional, Callable
+import sys
+from typing import Generator, Optional
 
 try:
     from rich.console import Console
+    from utils.error import SepheraError
+    from utils.utils import Utils
 except KeyboardInterrupt:
     print("\n Aborted by user.")
+    sys.exit(1)
 
-from utils.error import SepheraError
-
-"""" 
-Sephera Command Line Interface
-WalkFile class
-
-"""
 class WalkFile:
     def __init__(self, ignore_pattern: Optional[str] = None, base_path: str = ".") -> None:
         self.base_path = base_path
+        self.utils = Utils()
+
+        self.console = Console()
         self.ignore_regex: Optional[re.Pattern] = None
         self.ignore_str: Optional[str] = None
         
         if ignore_pattern:
             try:
                 self.ignore_regex = re.compile(ignore_pattern)
+
             except re.error:
                 self.ignore_regex = None
                 self.ignore_str = ignore_pattern
 
-    def _is_ignored(self, path: str) -> bool:
-        if self.ignore_regex:
-            return bool(self.ignore_regex.search(path))
-        
-        if self.ignore_str:
-            return self.ignore_str in path
-        
-        return False
-
     def walk_all_files(self) -> Generator[str, None, None]:
         for root, dirs, files in os.walk(self.base_path):
-            dirs[:] = [dir for dir in dirs if not self._is_ignored(os.path.join(root, dir))]
+            dirs[:] = [dir for dir in dirs 
+                       if not self.utils.is_ignored(
+                           path = os.path.join(root, dir),
+                           ignore_regex = self.ignore_regex,
+                           ignore_str = self.ignore_str
+                       )]
 
             for file in files:
                 file_path = os.path.join(root, file)
@@ -48,45 +45,47 @@ class WalkFile:
 
                 yield os.path.join(root, file)
 
-    def show_list_tree(self, on_step: Callable[[], None] = None, console: Console = None) -> dict[str, int]:
+    def show_list_tree(self) -> dict[str, int]:
         folder_count: int = 0
         file_count: int = 0
+
         hidden_file_count: int = 0
         hidden_folder_count: int = 0
         output: list[str] = []
 
-        for root, dirs, files in os.walk(self.base_path):
-            on_step()
-            for dir in list(dirs):
-                full_path = os.path.join(root, dir)
+        with self.console.status("[bold green] Processing...", spinner = "point") as progressBar:
+            for root, dirs, files in os.walk(self.base_path):
+                for dir in list(dirs):
+                    full_path = os.path.join(root, dir)
 
-                if self._is_ignored(full_path):
-                    dirs.remove(dir)
-                    continue
-                
-                if dir.startswith("."):
-                    hidden_folder_count += 1
-                on_step()
-                
-            for file in list(files):
-                full_path = os.path.join(root, file)
+                    if self.utils.is_ignored(path = full_path):
+                        dirs.remove(dir)
+                        continue
+                    
+                    if dir.startswith("."):
+                        hidden_folder_count += 1
+                    
+                for file in list(files):
+                    full_path = os.path.join(root, file)
 
-                if self._is_ignored(full_path):
-                    continue
+                    if self.utils.is_ignored(path = full_path, ignore_regex = self.ignore_regex, ignore_str = self.ignore_str):
+                        continue
 
-                if file.startswith("."):
-                    hidden_file_count += 1
-                else:
-                    file_count += 1
-                on_step()
+                    if file.startswith("."):
+                        hidden_file_count += 1
+                    else:
+                        file_count += 1
 
-            folder_count += len(dirs)
+                folder_count += len(dirs)
+            
+            self.console.clear()
 
-        self._show_list_tree(self.base_path, prefix = "", console = console, output = output)
+        self._show_list_tree(self.base_path, prefix = "", output = output)
         for line in output:
             print(f"{line}")
 
         print(f"{folder_count} Folder. {file_count} File.")
+        print(f"{hidden_folder_count} Hidden Folder. {hidden_file_count} Hidden File.")
 
         return {
             "Files": file_count,
@@ -95,11 +94,12 @@ class WalkFile:
             "Hidden_Directory": hidden_folder_count
         }
 
-    def _show_list_tree(self, current_dir: str, prefix: str, console: Console, output: list[str]) -> None:
+    def _show_list_tree(self, current_dir: str, prefix: str, output: list[str]) -> None:
         try:
             entries = sorted(os.listdir(current_dir))
+
         except PermissionError:
-            error = SepheraError(console = console)
+            error = SepheraError(self.console)
             error.show_error(f"Permission Denied. Skipping: {current_dir}")
             return
         
@@ -115,4 +115,4 @@ class WalkFile:
 
             if os.path.isdir(full_path):
                 extension = "    " if i == len(entries) - 1 else "│   "
-                self._show_list_tree(full_path, prefix + extension, console = console, output = output)
+                self._show_list_tree(full_path, prefix + extension, output = output)
