f2b5eb4cec7b2a7bcad78b831624a0675e940515
Reim-developer <contact.kaxtr@gmail.com>
2025-04-19 23:19:45 +0700

    Refactor Utils class to enhance path ignoring functionality and update return types for clarity

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,10 @@
 import re
 import os
-from typing import Optional
+from typing import Optional, List
+import fnmatch
 
 class Utils:
-    def is_ignored(self, path: str, ignore_regex: Optional[re.Pattern] = None, ignore_str: Optional[str] = None) -> None:
+    def is_ignored(self, path: str, ignore_regex: Optional[re.Pattern] = None, ignore_str: Optional[str] = None) -> bool:
         if ignore_regex:
             return bool(ignore_regex.search(path))
         
@@ -12,8 +13,38 @@ class Utils:
         
         return False
     
-    def is_hidden_path(self, path: str, base_path: str) -> None:
+    def is_multi_ignored(
+            self, path: str, ignore_regex: Optional[List[re.Pattern]] = None, 
+            ignore_str: Optional[List[str]] = None,
+            ignore_glob: Optional[List[str]] = None
+        ) -> bool:
+        if ignore_regex:
+            for regex in ignore_regex:
+                if regex.search(path):
+                    return True
+        
+        if ignore_str:
+            path_basename = os.path.basename(path)
+            ignore_str_set = set(ignore_str)
+            
+            for ignore_query in ignore_str_set:
+                if ignore_query == path_basename:
+                    return True
+        
+        if ignore_glob:
+            path_basename = os.path.basename(path)
+    
+            for glob in ignore_glob:
+                if fnmatch.fnmatch(path_basename, glob):
+                    return True
+
+        return False 
+    
+    def is_hidden_path(self, path: str, base_path: str) -> bool:
         rel_path = os.path.relpath(path, base_path)
         parts = rel_path.split(os.sep)
 
         return any(part.startswith(".") for part in parts)
+    
+    def is_path_exists(self, path: str) -> bool:
+        return os.path.exists(path = path)
