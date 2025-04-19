a96530ec239e1bbfbffd58dde2af06847aad9681
Reim-developer <contact.kaxtr@gmail.com>
2025-04-20 01:04:09 +0700

    Add config_data.py for language comment styles and extensions

---
+++ config_data.py
@@ -0,0 +1,199 @@
+# Auto-generated file config
+CONFIG_DATA = {
+  "comment_styles": {
+    "c_style": {
+      "single_line": "//",
+      "multi_line_start": "/*",
+      "multi_line_end": "*/"
+    },
+    "python_style": {
+      "single_line": "#",
+      "multi_line_start": "\"\"\"",
+      "multi_line_end": "\"\"\""
+    },
+    "shell_style": {
+      "single_line": "#"
+    },
+    "perl_style": {
+      "single_line": "#",
+      "multi_line_start": "=",
+      "multi_line_end": "=cut"
+    },
+    "ruby_style": {
+      "single_line": "#",
+      "multi_line_start": "=begin",
+      "multi_line_end": "=end"
+    },
+    "no_comment": {
+      "single_line": None,
+      "multi_line_start": None,
+      "multi_line_end": None
+    },
+    "html_style": {
+      "single_line": None,
+      "multi_line_start": "<!--",
+      "multi_line_end": "-->"
+    }
+  },
+  "languages": [
+    {
+      "name": "Python",
+      "extension": [
+        ".py"
+      ],
+      "comment_styles": "python_style"
+    },
+    {
+      "name": "Java",
+      "extension": [
+        ".java"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "JavaScript",
+      "extension": [
+        ".js",
+        ".jsx",
+        ".mjs"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "Shell Script",
+      "extension": [
+        ".sh"
+      ],
+      "comment_styles": "shell_style"
+    },
+    {
+      "name": "C++",
+      "extension": [
+        ".cc",
+        ".cpp",
+        ".cxx",
+        ".c++"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "Golang",
+      "extension": [
+        ".go"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "Perl",
+      "extension": [
+        ".pl"
+      ],
+      "comment_styles": "perl_style"
+    },
+    {
+      "name": "Ruby",
+      "extension": [
+        ".rb"
+      ],
+      "comment_styles": "ruby_style"
+    },
+    {
+      "name": "C Header File",
+      "extension": [
+        ".h"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "C++ Header File",
+      "extension": [
+        ".hpp",
+        ".hh",
+        ".h++"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "C#",
+      "extension": [
+        ".cs"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "TypeScript",
+      "extension": [
+        ".ts",
+        ".tsx"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "Rust",
+      "extension": [
+        ".rs"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "PHP",
+      "extension": [
+        ".php"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "YAML",
+      "extension": [
+        ".yml",
+        ".yaml"
+      ],
+      "comment_styles": "shell_style"
+    },
+    {
+      "name": "JSON",
+      "extension": [
+        ".json"
+      ],
+      "comment_styles": "no_comment"
+    },
+    {
+      "name": "Cython",
+      "extension": [
+        ".pyx",
+        ".pxd",
+        ".pxi"
+      ],
+      "comment_styles": "python_style"
+    },
+    {
+      "name": "CSS",
+      "extension": [
+        ".css"
+      ],
+      "comment_styles": "c_style"
+    },
+    {
+      "name": "HTML",
+      "extension": [
+        ".html",
+        ".htm"
+      ],
+      "comment_styles": "html_style"
+    },
+    {
+      "name": "XML",
+      "extension": [
+        ".xml"
+      ],
+      "comment_styles": "html_style"
+    },
+    {
+      "name": "Dart",
+      "extension": [
+        ".dart"
+      ],
+      "comment_styles": "c_style"
+    }
+  ]
+}
