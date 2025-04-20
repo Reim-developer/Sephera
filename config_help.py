36cfa4f6b4b11a5c398b6b579a7ebf1fba099307
Reim-developer <contact.kaxtr@gmail.com>
2025-04-20 22:02:50 +0700

    Add configuration help file with usage instructions

---
+++ config_help.py
@@ -0,0 +1,18 @@
+# ================================================================
+
+# Auto-generated file config from YAML configuration
+# You can customize this config via config/languages.yml file
+# If this file is not exists, you can find this in:
+# https://github.com/Reim-developer/Sephera/tree/master/config
+# This project is licensed under the GNU General Public License v3.0
+# https://github.com/Reim-developer/Sephera?tab=GPL-3.0-1-ov-file
+
+# ==============================================================
+
+MAIN_HELP = """Usage: sephera [options...]
+
+    loc                        Quickly calculate total lines of code in your project.
+    stats                      Show your project stats, size.
+    tree                       Show your project structure tree.
+
+Use `sephera --help [options...]` for more infomation."""
