f9767f7cd8780d38c7cd75ae88be3a191306251e
Reim-developer <contact.kaxtr@gmail.com>
2025-04-20 14:03:22 +0700

    Add comment, and fix UX in generate_config function

--- generate_config.py
+++ generate_config.py
@@ -10,7 +10,16 @@ def generate_config() -> None:
     config_json = re.sub(r'\bnull\b', 'None', config_json)
 
     with open(file = "config_data.py", mode = "w", encoding = "utf-8") as output_file:
-        output_file.write("# Auto-generated file config from YAML configuration.\n")
+        output_file.write("\n".join([
+            "# ================================================================",
+            "# Auto-generated file config from YAML configuration",
+            "# You can customize this config via config/languages.yml file",
+            "# If this file is not exists, you can find this in:",
+            "# https://github.com/Reim-developer/Sephera/tree/master/config",
+            "# This project is licensed under the GNU General Public License v3.0",
+            "# https://github.com/Reim-developer/Sephera?tab=GPL-3.0-1-ov-file",
+            "# ==============================================================\n"
+        ]))
         output_file.write(f"CONFIG_DATA = {config_json}\n")
 
     print(f"Generated config_data file successfully.")
