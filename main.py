5405632fab58c4664f03da0a39ee337b43ebe6cc
Reim-developer <contact.kaxtr@gmail.com>
2025-04-20 22:03:12 +0700

    Refactor SepheraCli to use custom SepheraArgparse for improved command line interface

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import sys
 
 try:
     from command import Command
+    from etc.subclass import SepheraArgparse
 except KeyboardInterrupt:
     print("\nAborted by user.")
     sys.exit(1)
 
 class SepheraCli:
     def __init__(self):
-        self.sephera_parser = argparse.ArgumentParser(description = "Sephera Commmand Line Interface")
+        self.sephera_parser = SepheraArgparse(
+            add_help = False,
+            usage = argparse.SUPPRESS,
+            formatter_class = argparse.RawDescriptionHelpFormatter
+        )
         command = Command(sephera_parser = self.sephera_parser)
         command.setup()
        
@@ -17,6 +22,9 @@ if __name__ == "__main__":
     try:
         cli = SepheraCli()
         args = cli.sephera_parser.parse_args()
-        args.function(args)
+        
+        if hasattr(args, 'function'):
+            args.function(args)
+    
     except KeyboardInterrupt:
         print("\n Aborted by user.")
