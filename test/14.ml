f9408379ae325f08996f2424cbb6c894077674d8
Reim-developer <contact.kaxtr@gmail.com>
2025-04-22 19:22:08 +0700

    Add initial implementation of comments and function structure in 14.ml

---
+++ test/14.ml
@@ -0,0 +1,8 @@
+(* single line comment *)
+
+(* multiple line comment, commenting out part of a program, and containing a
+nested comment:
+let f = function
+  | 'A'..'Z' -> "Uppercase"
+    (* Add other cases later... *)
+*)
