a6a781332890fc3a2a1a9924b99df7276ade023b
Reim-developer <contact.kaxtr@gmail.com>
2025-04-22 18:13:30 +0700

    Add fetch_latest_version and is_latest_version methods to Utils class for version checking

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,12 @@
 import re
 import os
-from typing import Optional, List
 import fnmatch
+import requests
+import sys
+from typing import Optional, List
+from rich.console import Console
+from __version__ import SEPHERA_VERSION
+from packaging import version
 
 class Utils:
     def is_ignored(self, path: str, ignore_regex: Optional[re.Pattern] = None, ignore_str: Optional[str] = None) -> bool:
@@ -47,3 +52,44 @@ class Utils:
     
     def is_path_exists(self, path: str) -> bool:
         return os.path.exists(path = path)
+
+    def fetch_latest_version(self) -> str:
+        console = Console()
+
+        GITHUB_REPO = "Reim-developer/Sephera"
+        GITHUB_API = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
+
+        request_headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+        }
+
+        try:
+            request = requests.get(url = GITHUB_API, headers = request_headers)
+
+        except Exception as error:
+            console.print("\n".join([
+                "[red][+] Error when fetch latest verion of Sephera:",
+                f"[red][+] Error name: {type(error).__name__}",
+                f"[red][+] Error details: [yellow]{error}"
+            ]))
+            sys.exit(1)
+
+        data = request.json()
+        
+        version_tag: str = data.get("tag_name", "")
+
+        return version_tag.lstrip("v")
+    
+    def is_latest_version(self) -> bool:
+        latest_version = version.parse(self.fetch_latest_version())
+        current_version = version.parse(SEPHERA_VERSION)
+
+        if latest_version > current_version or latest_version == current_version:
+            return True
+    
+        else:
+            return False
+
+
+
+        
