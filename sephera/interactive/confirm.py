9e227b48a75354771465a5603c5a74b28b01ccf4
Reim-developer <contact.kaxtr@gmail.com>
2025-04-23 20:07:46 +0700

    Add override_write_confirm method for file overwrite confirmation

--- sephera/interactive/confirm.py
+++ sephera/interactive/confirm.py
@@ -29,4 +29,31 @@ class ConfirmInteractive:
                 case _: return False
 
         except KeyboardInterrupt:
-            return False            
+            return False
+
+    def override_write_confirm(self, file_name: str) -> bool:
+        try:
+            while True:
+                self.console.print("\n".join([
+                    f"[yellow][!] Your file {file_name} is already exists. Do you want",
+                    f"[cyan][1] Override all data in {file_name}.",
+                    f"[cyan][2] No override. Cancel write data, and exit now.",
+                    f"[yellow][!] Default as 1 if you leave blank."
+                ]))
+
+                option: str = input("Your option [1, 2]: ").strip()
+
+                if not option:
+                    return True
+                
+                match option:
+                    case "1": return True
+                    case "2": return False
+
+                    case _:
+                        self.console.print(f"[red] Invalid option: {option}")
+
+        except KeyboardInterrupt:
+            self.console.print("\n[cyan][+] Aborted by user.")
+            return False
+                
