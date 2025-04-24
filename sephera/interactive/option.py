8b3531a00e78e3ef62098ca297cc7df2e604060a
Reim-developer <contact.kaxtr@gmail.com>
2025-04-24 18:29:16 +0700

    Apply logic changes in CodeLoc class, and fix UI, optimize performance in on_export_option. Fix exit logic in _show_success_msg and _show_override_msg, migration sys.exit() to on_export_option. It will optimize code logic

--- sephera/interactive/option.py
+++ sephera/interactive/option.py
@@ -1,5 +1,7 @@
 import sys 
 import os
+import logging
+import time
 
 try:
     from rich.console import Console
@@ -12,10 +14,15 @@ class OptionHandler:
     def __init__(self) -> None:
         self.console = Console()
         self.confirmInteractive = ConfirmInteractive()
+        logging.basicConfig(level = logging.INFO, format = "[%(levelname)s] %(message)s")
 
-    
     def on_json_export_option(self, args: str) -> None:
-        codeLoc = CodeLoc(args.path, args.ignore)
+        start_time = time.perf_counter()
+        with self.console.status(status = "Processing...", spinner = "material"):
+            codeLoc = CodeLoc(args.path, args.ignore)
+
+        end_time = time.perf_counter()
+        self.console.clear()
 
         if not args.json.endswith(".json"):
                 args.json += ".json"
@@ -23,20 +30,27 @@ class OptionHandler:
         if os.path.exists(args.json):
             confirm_override: bool = self.confirmInteractive.override_write_confirm(file_name = args.json)
 
+            if not confirm_override:
+                sys.exit(0)
+
             if confirm_override:
                 codeLoc.export_to_json(file_path = args.json)
+                logging.info(f"Finished in {end_time - start_time:.2f}s")
                 self.console.clear()
 
                 verbose_confirm = self.confirmInteractive.verbose_confirm()
                 self._show_override_msg(
                     verbose_confirm = verbose_confirm, codeLoc = codeLoc, args = args
                 )
+                logging.info(f"Finished in {end_time - start_time:.2f}s")
+                sys.exit(0)
 
         codeLoc.export_to_json(file_path = args.json)
         self.console.clear()
 
         verbose_confirm_2 = self.confirmInteractive.verbose_confirm()
         self._show_success_msg(verbose_confirm = verbose_confirm_2, codeLoc = codeLoc, args = args)
+        logging.info(f"Finished in {end_time - start_time:.2f}s")
 
     def _show_override_msg(self, verbose_confirm: bool, codeLoc: CodeLoc, args: str) -> None:
         if verbose_confirm:
@@ -45,14 +59,12 @@ class OptionHandler:
                 f"Override file {args.json} successfully.",
                 f"File path directory: {os.path.abspath(args.json)}"
             ]))
-            sys.exit(0)
 
         else:
             self.console.print("\n".join([
                 f"Override file {args.json} successfully.",
                 f"File path directory: {os.path.abspath(args.json)}"
             ]))
-            sys.exit(0)
 
     def _show_success_msg(self, verbose_confirm: bool, codeLoc: CodeLoc, args: str) -> None:
         if verbose_confirm:
@@ -67,4 +79,3 @@ class OptionHandler:
                 f"Sucessfully save {args.json}.",
                 f"Save directory path: {os.path.abspath(args.json)}"
             ]))
-            sys.exit(0)
