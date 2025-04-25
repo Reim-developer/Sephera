ee584e92398084300ecac526878b00d2fe7063fc
Reim-developer <contact.kaxtr@gmail.com>
2025-04-25 17:39:10 +0700

    Refactor GetUpdate class: integrate latest_version_option logic into update_sephera method and remove redundant code.

--- sephera/get_update.py
+++ sephera/get_update.py
@@ -3,6 +3,9 @@ import sys
 try:
     from utils.utils import Utils
     from rich.console import Console
+    from sephera.interactive.confirm import ConfirmInteractive
+    from sephera.interactive.option import OptionHandler
+    from sephera.net.network_helper import NetworkHelper
 except KeyboardInterrupt:
     print("\n Aborted by user.")
 
@@ -10,30 +13,9 @@ class GetUpdate:
     def __init__(self) -> None:
         self.utils = Utils()
         self.console = Console()
-        
-    def _latest_version_option(self) -> None:
-        while True:
-            self.console.print("\n".join([
-                "[yellow][!] You're using latest version of Sephera, do you want:",
-                "[cyan][1] Re-install Sephera.",
-                "[cyan][2] Install to another directory path.",
-                "[cyan][3] Cancel and exit now."
-            ]))
-            prompt_value: str = input("Your option [1-3]: ").strip()
-
-            match prompt_value:
-                case "1":
-                    # Todo: Re-install Sephera logic here
-                    pass
-
-                case "2":
-                    # Todo: Re-install Sephera in another directory path here
-                    pass
-
-                case "3": sys.exit(0)
-
-                case _:
-                    self.console.print(f"[red]Invalid option: {prompt_value}. Type '3' to exit.")
+        self.confirm_interactive = ConfirmInteractive()
+        self.network = NetworkHelper()
+        self.option_interactive = OptionHandler()
                     
     def update_sephera(self) -> None:
         try:
@@ -49,7 +31,20 @@ class GetUpdate:
 
         if is_latest_version:
             try:
-                self._latest_version_option()
+                user_option = self.confirm_interactive.latest_version_option()
+
+                match user_option:
+                    case self.confirm_interactive.RE_INSTALL_CONFIRM:
+                        self.network.install_sephera()
+
+                    case self.confirm_interactive.INSTALL_TO_ANOTHER_PATH:
+                        dir_path = self.option_interactive.on_choose_dir_path()
+
+                        if dir_path: 
+                            self.network.install_sephera(save_dir= dir_path)
+
+                    case self.confirm_interactive.EXIT_CONFIRM:
+                        sys.exit(0)
             
             except KeyboardInterrupt:
                 self.console.print("\n[cyan][+] Aborted by user.")
