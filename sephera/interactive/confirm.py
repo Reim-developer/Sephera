573a9f09ce01de75357814cf6c5fa936f73fd2ee
Reim-developer <contact.kaxtr@gmail.com>
2025-04-25 17:39:13 +0700

    Add latest_version_option method to ConfirmInteractive class for user prompts on version actions

--- sephera/interactive/confirm.py
+++ sephera/interactive/confirm.py
@@ -9,6 +9,9 @@ except KeyboardInterrupt:
 class ConfirmInteractive:
     def __init__(self) -> None:
         self.console = Console()
+        self.RE_INSTALL_CONFIRM: str = "re_install"
+        self.INSTALL_TO_ANOTHER_PATH: str = "install_to_another_path"
+        self.EXIT_CONFIRM: str = "confirm_exit"
     
     def verbose_confirm(self) -> bool:
         try:
@@ -30,6 +33,29 @@ class ConfirmInteractive:
 
         except KeyboardInterrupt:
             return False
+        
+    def latest_version_option(self) -> str:
+        while True:
+            self.console.print("\n".join([
+                "[yellow][!] You're using latest version of Sephera, do you want:",
+                "[cyan][1] Re-install Sephera.",
+                "[cyan][2] Install to another directory path.",
+                "[cyan][3] Cancel and exit now."
+            ]))
+            prompt_value: str = input("Your option [1-3]: ").strip()
+
+            match prompt_value:
+                case "1":
+                    return self.RE_INSTALL_CONFIRM
+
+                case "2":
+                    return self.INSTALL_TO_ANOTHER_PATH
+
+                case "3":
+                    return self.EXIT_CONFIRM
+
+                case _:
+                    self.console.print(f"[red]Invalid option: {prompt_value}. Type '3' to exit.")
 
     def override_write_confirm(self, file_name: str) -> bool:
         try:
