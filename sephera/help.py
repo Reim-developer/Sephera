7f6950dcfb9e1aa8d9146973d51f5748167296e1
Reim-developer <contact.kaxtr@gmail.com>
2025-04-27 01:17:32 +0700

    Refactor help command imports and add version and cfg-language command help

--- sephera/help.py
+++ sephera/help.py
@@ -1,9 +1,10 @@
 import sys
 try:
     from etc.generate.config_help import (
-        LOC_COMMAND_HELP, STATS_COMMAND_HELP, 
-        TREE_COMMAND_HELP, UPDATE_COMMAND_HELP,
-        LANGUAGE_SUPPORT_COMMAND_HELP, MAIN_HELP
+        MAIN_HELP, LOC_COMMAND_HELP, 
+        STATS_COMMAND_HELP, TREE_COMMAND_HELP, 
+        UPDATE_COMMAND_HELP, LANGUAGE_SUPPORT_COMMAND_HELP,
+        VERSION_COMMAND_HELP, SET_CFG_COMMAND_HELP
     )
     from rich.console import Console
 except KeyboardInterrupt:
@@ -37,9 +38,15 @@ class SepheraHelp:
             case "language-support":
                 self.console.print(f"[cyan]{LANGUAGE_SUPPORT_COMMAND_HELP}")
 
+            case "version":
+                self.console.print(f"[cyan]{VERSION_COMMAND_HELP}")
+            
+            case "cfg-language":
+                self.console.print(f"[cyan]{SET_CFG_COMMAND_HELP}")
+
             case _:
                 self.console.print("\n".join([
-                    f"[red]Invalid option: {args}",
-                    "[yellow]Hint: Allowed options: [cyan]loc, stats, tree, update"
+                    f"[red][!] Invalid option: {args}",
+                    "[yellow][+] Hint: Allowed options: [cyan]loc, stats, tree, update, language-support, version, cfg-language"
                 ]))
                 sys.exit(1)
