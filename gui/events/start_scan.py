38f7448e751de7e3a996144c3c23b09165e50b35
Reim-developer <contact.kaxtr@gmail.com>
2025-04-29 23:18:48 +0700

    Refactor StartScanEvent to include sorting functionality and update item handling in the result display

--- gui/events/start_scan.py
+++ gui/events/start_scan.py
@@ -1,11 +1,15 @@
 import os
 import logging
 from sephera.CodeLoc import CodeLoc
+from gui.etc.subclass import (
+    TableWidgetSubclass, TableWidgetFloat,
+    QTableWidgetSubclass
+)
 from PyQt5.QtCore import (
-    QRunnable, QThreadPool, pyqtSignal, QObject, pyqtSlot
+    QRunnable, QThreadPool, pyqtSignal, QObject, pyqtSlot, Qt
 )
 from PyQt5.QtWidgets import (
-    QLineEdit, QTableWidget, QTableWidgetItem, QProgressBar, QMessageBox
+    QLineEdit, QTableWidgetItem, QProgressBar, QMessageBox,
 )
 
 class ScanWorkerSignals(QObject):
@@ -22,11 +26,46 @@ class ScanWorker(QRunnable):
         codeLoc = CodeLoc(base_path=self.path)
         self.signals.finished.emit(codeLoc)
 
+
 class StartScanEvent:
     def __init__(self):
         self.threadpool = QThreadPool()
+        self._sort_mode: str | None = None
 
-    def show_result(self, table_widget: QTableWidget, codeLoc: CodeLoc):
+    def set_sort_mode(self, mode: str) -> None:
+        self._sort_mode = mode
+
+    def _auto_sort(self, table: QTableWidgetSubclass) -> None:
+         match self._sort_mode:
+            # Most, least lines of code
+            case "Most lines of code":
+                table.sortItems(1, Qt.SortOrder.DescendingOrder)
+
+            case "Least lines of code":
+                table.sortItems(1, Qt.SortOrder.AscendingOrder)
+
+            # Most, least lines of comment
+            case "Most lines of comment":
+                table.sortItems(2, Qt.SortOrder.DescendingOrder)
+
+            case "Least lines of comment":
+                table.sortItems(2, Qt.SortOrder.AscendingOrder)
+
+            # Most, least empty lines
+            case "Most lines of empty":
+                table.sortItems(3, Qt.SortOrder.DescendingOrder)
+
+            case "Least lines of empty":
+                table.sortItems(3, Qt.SortOrder.AscendingOrder)
+            
+            # Most, least sizeof
+            case "Most size":
+                table.sortItems(4, Qt.SortOrder.DescendingOrder)
+
+            case "Least size":
+                table.sortItems(4, Qt.SortOrder.AscendingOrder)
+
+    def show_result(self, table_widget: QTableWidgetSubclass, codeLoc: CodeLoc):
         table_widget.setColumnCount(5)
         table_widget.setHorizontalHeaderLabels([
             "Language", "Code lines", "Comment lines", "Empty lines", "Size (MB)"
@@ -44,15 +83,15 @@ class StartScanEvent:
 
             if loc_line > 0 or comment_line > 0 or empty_line > 0 or total_sizeof > 0:
                 lang_config = codeLoc.language_data.get_language_by_name(name=language)
-                comment_result = "N/A" if lang_config.comment_style == "no_comment" else str(comment_line)
+                comment_result = 0 if lang_config.comment_style == "no_comment" else str(comment_line)
 
                 table_widget.setItem(row, 0, QTableWidgetItem(language))
 
-                table_widget.setItem(row, 1, QTableWidgetItem(str(loc_line)))
-                table_widget.setItem(row, 2, QTableWidgetItem(comment_result))
+                table_widget.setItem(row, 1, TableWidgetSubclass(str(loc_line)))
+                table_widget.setItem(row, 2, TableWidgetSubclass(str(comment_result)))
 
-                table_widget.setItem(row, 3, QTableWidgetItem(str(empty_line)))
-                table_widget.setItem(row, 4, QTableWidgetItem(f"{total_sizeof:.2f}"))
+                table_widget.setItem(row, 3, TableWidgetSubclass(str(empty_line)))
+                table_widget.setItem(row, 4, TableWidgetFloat(f"{total_sizeof:.2f}"))
 
                 total_loc_count += loc_line
                 total_comment += comment_line
@@ -65,20 +104,22 @@ class StartScanEvent:
 
         table_widget.setRowCount(row)
 
+        self._auto_sort(table = table_widget)
+
     def set_start_scan_event(self, text_line: QLineEdit, 
-                             table_widget: QTableWidget, progress: QProgressBar) -> None:
+                             table_widget: QTableWidgetSubclass, progress: QProgressBar) -> None:
         project_path = text_line.text()
 
         if not os.path.exists(project_path):
-            msg = QMessageBox()
-            msg.setWindowTitle("Not found")
+            dialog = QMessageBox()
+            dialog.setWindowTitle("Not found")
 
-            msg.setText("Directory or project path not found.")
-            msg.exec()
+            dialog.setText("Directory or project path not found.")
+            dialog.exec()
 
             logging.warning("Directory or project path not found.")
             return
-
+        
         progress.setRange(0, 0)
         worker = ScanWorker(project_path)
 
