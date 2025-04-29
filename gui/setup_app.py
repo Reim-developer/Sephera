ad4ee498122fb598729b843bef4b4976f2190a0e
Reim-developer <contact.kaxtr@gmail.com>
2025-04-29 23:18:31 +0700

    Add sorting menu and connect actions in setup_app

--- gui/setup_app.py
+++ gui/setup_app.py
@@ -1,11 +1,13 @@
 from PyQt5.QtWidgets import (
     QPushButton, QWidget, QApplication, QLineEdit,
-    QTableWidget, QProgressBar
+    QProgressBar, QMenu
 )
 from PyQt5.QtGui import QPalette
 from gui.gui_utils.utils import GuiUtils
 from gui.events.open_project import OpenProjectEvent
 from gui.events.start_scan import StartScanEvent
+from gui.events.menu import MenuEvent
+from gui.etc.subclass import QTableWidgetSubclass
 
 class Setup:
     def __init__(self, widget: QWidget):
@@ -16,13 +18,16 @@ class Setup:
         self.start_scan_btn = QPushButton()
 
         self.show_project_line = QLineEdit()
-        self.show_result_table = QTableWidget()
+        self.show_result_table = QTableWidgetSubclass()
 
         self.win_palette = QPalette()
         self.progress_bar = QProgressBar()
 
         self.open_prj_event = OpenProjectEvent()
         self.scan_prj_event = StartScanEvent()
+
+        self.sort_by_menu = QMenu()
+        self.menu_event = MenuEvent(scanEvent = self.scan_prj_event)
 
     def setup_event(self) -> None:
         self.open_project_btn.clicked.connect(
@@ -34,6 +39,14 @@ class Setup:
                 self.scan_prj_event.set_start_scan_event(
                     text_line = self.show_project_line, 
                     table_widget = self.show_result_table, progress = self.progress_bar)
+        )
+
+        self.sort_by_menu.triggered.connect(
+            lambda action:
+                self.menu_event.set_menu_action(
+                    action,
+                    self.gui_utils.menu_button, self.show_result_table
+                )
         )
 
     def setup_application(self, app: QApplication) -> None:
@@ -68,6 +81,17 @@ class Setup:
         self.gui_utils.set_progress_bar(
             widget = self.widget, progress = self.progress_bar,
             geometry = (300, 420, 250, 30)
+        )
+
+        self.gui_utils.set_menu(
+            widget = self.widget, menu = self.sort_by_menu,
+            text = [
+                "Most lines of code", "Least lines of code",
+                "Most lines of comment", "Least lines of comment",
+                "Most lines of empty", "Least lines of empty",
+                "Most size", "Least size"
+            ], geometry = (0, 50, 150, 30),
+            button_text = "Sort by..."
         )
 
         self.setup_event()
