45dd45642f4065e14668d08cbda6de771a594fba
Reim-developer <contact.kaxtr@gmail.com>
2025-04-30 20:10:56 +0700

    Add new event 'export_action' to menu section events

--- gui/events/menu.py
+++ gui/events/menu.py
@@ -1,14 +1,35 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QPushButton, QAction
+from PyQt5.QtWidgets import QPushButton, QAction, QMessageBox
 from gui.etc.subclass import QTableWidgetSubclass
 from gui.events.start_scan import StartScanEvent
 
 class MenuEvent:
     def __init__(self, scanEvent: StartScanEvent = None):
         self.scanEvent = scanEvent
+
+    def set_export_acction(self, action: QAction, 
+                           button: QPushButton, table: QTableWidgetSubclass) -> None:
+        if table.rowCount() == 0:
+            dialog = QMessageBox()
+            dialog.setWindowTitle("Not found.")
+
+            dialog.setText("Table is none. Start scan to export")
+            dialog.exec()
+
+            return
+
+        button.setText(action.text())
+
+        match action.text():
+            case "Export to JSON":
+                # Add export to JSON soon.
+                pass
+
         
-    def set_menu_action(self, action: QAction, 
+
+    def set_sort_action(self, action: QAction, 
                         button: QPushButton, table: QTableWidgetSubclass) -> None:
+
         button.setText(action.text())
         self.scanEvent.set_sort_mode(action.text())
 
