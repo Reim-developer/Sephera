285098b21855e064db3bd74efdc0ce7d03d6581f
Reim-developer <contact.kaxtr@gmail.com>
2025-05-01 12:04:07 +0700

    Add SepheraWindow class implementation for main application window

---
+++ Source/sephera.cc
@@ -0,0 +1,11 @@
+#include "gui/include/sephera.hpp"
+
+using namespace sephera_cpp::gui;
+
+SepheraWindow::SepheraWindow(QWidget *widget) : QMainWindow(widget) {
+    setWindowTitle("Sephera-cpp");
+}
+
+SepheraWindow::~SepheraWindow() {
+
+}
