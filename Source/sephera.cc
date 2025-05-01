574847a61e350dbe35c29eb4e79a4f317612ccd0
Reim-developer <contact.kaxtr@gmail.com>
2025-05-01 16:15:54 +0700

    Refactor SepheraWindow constructor for improved layout initialization

--- Source/sephera.cc
+++ Source/sephera.cc
@@ -1,11 +1,26 @@
 #include "gui/include/sephera.hpp"
+#include "gui/include/init.hpp"
 
 using namespace sephera_cpp::gui;
 
 SepheraWindow::SepheraWindow(QWidget *widget) : QMainWindow(widget) {
     setWindowTitle("Sephera-cpp");
+    
+    resize(800, 600);
+    setMinimumSize(550, 400);
+
+    centralWidget = new QWidget(this);
+    layout = new QGridLayout();
+    
+    setCentralWidget(centralWidget);
+    centralWidget->setLayout(layout);
+
+    Init *init = new Init();
+    
+    init->setupGui(this);
+    init->setupContext();
 }
 
 SepheraWindow::~SepheraWindow() {
-
+  
 }
