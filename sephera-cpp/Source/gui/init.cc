187bace7c20cac73cb341db7076cba92cdcec25e
Reim-developer <contact.kaxtr@gmail.com>
2025-05-01 23:15:25 +0700

    Refactor Init class: update setupGui method to improve layout management and button positioning

--- sephera-cpp/Source/gui/init.cc
+++ sephera-cpp/Source/gui/init.cc
@@ -16,26 +16,44 @@ Init::Init() {
   open_project_btn = new QPushButton();
   scan_project_btn = new QPushButton();
   option_btn = new QPushButton();
+  sort_btn = new QPushButton();
+
+  resultTable = new QTableWidget();
 }
 
 void Init::setupGui(SepheraWindow *sepheraWindow) {
+  components->setPushButton(sepheraWindow, sort_btn, sepheraWindow->layout, 30,
+                            30, 0, 0, "Sort by...");
+  components->setPushButton(sepheraWindow, option_btn, sepheraWindow->layout,
+                            30, 30, 0, 1, "Options");
 
-  components->setSpacerItem(sepheraWindow, 20, 20, 1, 1);
+  components->setTableWidget(sepheraWindow, resultTable, sepheraWindow->layout,
+                             150, 40, 1, 0);
+  sepheraWindow->layout->removeWidget(resultTable);
+  sepheraWindow->layout->addWidget(resultTable, 1, 0, 1, 3);
 
-  components->setLineEdit(displayPathTextbox, sepheraWindow->layout, 150, 30, 4,
-                         1);
+  components->setLabel(displayPathDescription, sepheraWindow->layout, 150, 20,
+                       2, 0, "Input project path or section project");
+  displayPathDescription->setWordWrap(true);
+  sepheraWindow->layout->removeWidget(displayPathDescription);
+  sepheraWindow->layout->addWidget(displayPathDescription, 2, 0, 1, 2);
 
-  components->setLabel(displayPathDescription, sepheraWindow->layout, 150, 30, 3,
-                      1, QString("Input project path or section project"));
+  components->setLineEdit(displayPathTextbox, sepheraWindow->layout, 150, 30, 3,
+                          0);
+  sepheraWindow->layout->removeWidget(displayPathTextbox);
+  sepheraWindow->layout->addWidget(displayPathTextbox, 3, 0, 1, 2);
 
   components->setPushButton(sepheraWindow, open_project_btn,
-                           sepheraWindow->layout, 30, 30, 4, 2, "Open Project");
-
+                            sepheraWindow->layout, 30, 30, 3, 2,
+                            "Open Project");
   components->setPushButton(sepheraWindow, scan_project_btn,
-                           sepheraWindow->layout, 30, 30, 4, 3, "Scan Project");
-
-  components->setPushButton(sepheraWindow, option_btn, sepheraWindow->layout, 30,
-                           30, 0, 3, "Options");
+                            sepheraWindow->layout, 30, 30, 3, 3,
+                            "Scan Project");
 }
 
-void Init::setupContext() { context->setOptionMenuBehavior(this->option_btn); }
+void Init::setupContext(SepheraWindow *sepheraWindow) {
+  context->setOptionMenuBehavior(this->option_btn);
+
+  context->setScanButtonBehavior(sepheraWindow, scan_project_btn,
+                                 displayPathTextbox, resultTable);
+}
