297cacec906c3385efcf8b65dbcfc21260d19399
Reim-developer <contact.kaxtr@gmail.com>
2025-05-01 17:31:43 +0700

    Add cppbuild.sh script for building Sephera C++ with environment checks

---
+++ scripts/cppbuild.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+os_type=$(uname -s)
+program_name="sephera-cpp"
+cfg_path="../sephera-cpp/config.yml"
+build_dir="../sephera-cpp/build"
+exit_code=$?
+
+clang_detect() {
+    if command -v clang >/dev/null 2>&1; then
+        echo "[OK] Clang is already install in your os..."
+    else
+        echo "[BAD] Clang not found in your os. Please install them before build."
+        exit 1
+    fi
+}
+
+cmake_detect() {
+    if command -v cmake >/dev/null 2>&1; then
+        echo "[OK] Cmake is already install in your os..."
+    else
+        echo "[BAD] CMake not found in your os. Please install them before build."
+        exit 1
+    fi
+}
+
+os_detect() {
+    case $os_type in "Linux")
+        clang_detect
+        cmake_detect
+        
+        if [ ! -d "$build_dir" ]; then
+            echo "$build_dir doesn't exists. Create now.."
+            mkdir -p "$build_dir"
+        fi
+
+        cp "$cfg_path" "$build_dir"
+        cd "$build_dir" || exit 1
+        cmake ..
+        make 
+        ./"$program_name"
+
+        echo "Build SUCCESS | With exit status code: $exit_code"
+    ;;
+        
+    esac
+}
+
+os_detect
