2250fd4acf7db4ba2337278d3725a2eb13857772
Reim-developer <contact.kaxtr@gmail.com>
2025-05-01 16:16:00 +0700

    Add context.hpp to define SepheraContext class and setOptionMenuBehavior method

---
+++ Source/context/include/context.hpp
@@ -0,0 +1,14 @@
+#ifndef CONTEXT_HPP
+#define CONTEXT_HPP
+#include <QPushButton>
+
+namespace sephera_cpp::context {
+    class SepheraContext {
+        public:
+            void setOptionMenuBehavior(QPushButton *button);
+    };
+
+} // namespace sephera_cpp::context
+
+
+#endif // CONTEXT_HPP
