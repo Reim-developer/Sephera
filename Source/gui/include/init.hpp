6aa15088f5da0815e4a0d41e33350c1159d149fa
Reim-developer <contact.kaxtr@gmail.com>
2025-05-01 16:16:15 +0700

    Add init.hpp to define the Init class for GUI setup and context management

---
+++ Source/gui/include/init.hpp
@@ -0,0 +1,33 @@
+#ifndef INIT_HPP
+#define INIT_HPP
+#include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
+#include "../../context/include/context.hpp"
+#include "components.hpp"
+
+using namespace sephera_cpp::context;
+
+namespace sephera_cpp::gui {
+    class Init {
+        public:
+            Init();
+            void setupGui(SepheraWindow *sepheraWindow);
+
+            void setupContext();
+
+        private:
+            Components components;
+            SepheraContext *context;
+
+        public:
+            QLineEdit *displayPathTextbox;
+            QLabel *displayPathDescription;
+            QPushButton *open_project_btn;
+            QPushButton *scan_project_btn;
+            QPushButton *option_btn;
+    };
+
+} // namespace sephera_cpp::gui
+
+#endif // INIT_HPP
