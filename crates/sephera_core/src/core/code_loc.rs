bb66e7a904f722fe7215b5552cca964278cc4973
Reim-developer <contact.kaxtr@gmail.com>
2026-03-24 04:44:57 +0700

    refactor(sephera_core): update IgnoreMatcher export path to absolute crate path

--- crates/sephera_core/src/core/code_loc.rs
+++ crates/sephera_core/src/core/code_loc.rs
@@ -1,14 +1,13 @@
-﻿#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::module_name_repetitions)]
 
 mod analyzer;
-mod ignore;
 mod reader;
 mod scanner;
 #[cfg(test)]
 mod tests;
 mod types;
 
+pub use crate::core::ignore::IgnoreMatcher;
 pub use analyzer::CodeLoc;
-pub use ignore::IgnoreMatcher;
 pub use scanner::scan_content;
 pub use types::{CodeLocReport, LanguageLoc, LocMetrics};
