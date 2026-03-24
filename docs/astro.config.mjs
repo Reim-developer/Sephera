6106ce3b8a0cd1e86fe318f6435133896c2f3817
Reim-developer <contact.kaxtr@gmail.com>
2026-03-24 14:20:58 +0700

    config: add site URL to Astro docs configuration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+	site: "https://sephera.vercel.app",
 	integrations: [
 		starlight({
 			title: 'Sephera Docs',
